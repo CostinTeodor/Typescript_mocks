4af9c2a99d7b57b94689a546eba206cdc902a822
Teo Costin <teocostin23@yahoo.com>
2023-04-27 10:22:03 +0300

    mocked lambdaTest method

--- 01-mocking-simple-lambda/test/main.test.ts
+++ 01-mocking-simple-lambda/test/main.test.ts
@@ -1,16 +1,24 @@
-class MockedMyClass{
+import { MyClass } from "../src/main";
+
+class MockedMyClass {
   myAttribute!: (param: string) => any;
+
   lambdaTest(fn: (param: string) => any) {
     this.myAttribute = fn;
   }
 }
 
 describe("MockedMyClass", () => {
-  const myObj = new MockedMyClass();
   test("testCase1", () => {
-    myObj.lambdaTest((input) => {
-      return input;
-    });
-    expect(myObj.myAttribute("Beta")).toBe("Beta");
+    const mockedClass = new MockedMyClass();
+    mockedClass.myAttribute = jest.fn((param: string) => param);
+
+    const myClass = new MyClass();
+
+    const lambdaTestMock = jest.fn((fn) => fn("Beta"));
+
+    jest.spyOn(myClass, "lambdaTest").mockImplementation(lambdaTestMock);
+
+    expect(mockedClass.myAttribute("Beta")).toBe("Beta");
   });
 });
